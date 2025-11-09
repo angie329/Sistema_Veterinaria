52e7222c2a8d57b0ace9938ae4b69c7f06253aba
sgmunoz2 <smunozsa@fiec.espol.edu.ec>
2025-11-08 23:32:30 -0500

    exportar backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -20,6 +20,7 @@ import { productsRouter } from './products.routes.js';
 import { movementsRouter } from './movements.routes.js';
 import { incomesRouter } from './incomes.routes.js';
 import { salidasRouter } from './salidas.routes.js';
+import { invExportsRouter } from "./invExports.routes.js";
 export const router = express.Router();
 
 router.get("/health", (_, res) => res.send("OK"));
@@ -43,3 +44,4 @@ router.use('/products', productsRouter);
 router.use('/movements', movementsRouter);
 router.use('/incomes', incomesRouter);
 router.use('/salidas', salidasRouter);
+router.use('/invexports', invExportsRouter);
